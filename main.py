bfee2a93bea435124b3bc97b8f1cb9d1e31ff94e
Adam Berman <adam@returntocorp.com>
2026-03-08 20:35:10 -0700

    other

--- main.py
+++ main.py
@@ -148,6 +148,39 @@ def get_greenhouse_candidates(api_token: str, candidate_ids: List[int]) -> Dict:
             page += 1
     return candidates
 
+def get_culture_amp_token(client_id: str, client_secret: str) -> str:
+    response = requests.post(
+        "https://api.cultureamp.com/v1/oauth2/token",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data={
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "scope": "target-entity:8ed17dce-9eca-4383-a9e1-54f82c362b6d:employees-read,performance-evaluations-read"
+        }
+    )
+    response.raise_for_status()
+    return response.json()["access_token"]
+
+def fetch_culture_amp_employees(client_id: str, client_secret: str) -> Dict:
+    access_token = get_culture_amp_token(client_id, client_secret)
+    employees = []
+    pagination = ""
+    while True:
+        response = requests.get(
+            f"https://api.cultureamp.com/v1/employees{pagination}",
+            headers={"Authorization": f"Bearer {access_token}"}
+        )
+        response.raise_for_status()
+        data = response.json()
+        employees.extend(data["employees"])
+        if "pagination" in data and "afterKey" in data["pagination"]:
+            pagination = f"?afterKey={data['pagination']['afterKey']}"
+        else:
+            break
+
+    return employees
+
 def format_scorecard(scorecard: Dict) -> Dict:
     return {
         "created_at": scorecard["created_at"],
@@ -164,85 +197,96 @@ def format_scorecard(scorecard: Dict) -> Dict:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--use-applications", action="store_true", help="Use applications data")
+    parser.add_argument("--skip-greenhouse", action="store_true", help="Skip greenhouse data")
     args = parser.parse_args()
     use_applications = args.use_applications
+    skip_greenhouse = args.skip_greenhouse
 
     api_token = os.getenv("GREENHOUSE_API_TOKEN")
-    job_dicts = []
-    if use_applications:
-        applications = get_greenhouse_applications(api_token)
-        print(f"Found {len(applications)} applications")
-        job_dicts = [{"application_id": application["id"], "candidate_id": application["candidate_id"]} for application in applications]
-    else:
-        accepted_offers = get_greenhouse_accepted_offers(api_token)
-        print(f"Found {len(accepted_offers)} accepted offers")
-        job_dicts = [{"application_id": offer["application_id"], "candidate_id": offer["candidate_id"]} for offer in accepted_offers]
-    scorecard_data = {}
-    candidate_data = {}
-    
-    # Function to process a single offer
-    def process_offer(application_id: int):
-        return application_id, get_greenhouse_scorecards(api_token, application_id)
-    
-    # Use ThreadPoolExecutor to parallelize API calls
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        print("Processing offers in parallel...")
-        future_to_offer = {executor.submit(process_offer, job_dict["application_id"]): job_dict for job_dict in job_dicts}
-        
-        completed = 0
-        for future in concurrent.futures.as_completed(future_to_offer):
-            completed += 1
-            if completed % 10 == 0:
-                print(f"Processed {completed} of {len(job_dicts)} offers")
-            try:
-                application_id, scorecards = future.result()
-                scorecard_data[application_id] = scorecards
-            except Exception as exc:
-                print(f'Offer processing generated an exception: {exc}')
-    
-    candidate_ids = [job_dict["candidate_id"] for job_dict in job_dicts]
-    candidates = get_greenhouse_candidates(api_token, candidate_ids)
-    for candidate in candidates:
-        candidate_data[candidate["id"]] = candidate
-    print(f"Found {len(scorecard_data)} scorecards and {len(candidate_data)} candidates")
-    candidates_to_offers = {}
-    for candidate_id in candidate_ids:
-        if candidate_id not in candidates_to_offers:
-            candidate_name = candidate_data[candidate_id]["first_name"] + " " + candidate_data[candidate_id]["last_name"]
-            candidates_to_offers[candidate_id] = { "name": candidate_name, "offers": 1}
-        else:
-            candidates_to_offers[candidate_id]["offers"] += 1
-    for candidate_id, data in candidates_to_offers.items():
-        if data["offers"] > 1:
-            print(f"{data['name']} has {data['offers']} offers")
-        else:
-            print(f"{data['name']}")
+    culture_amp_client_id = os.getenv("CULTURE_AMP_CLIENT_ID")
+    culture_amp_client_secret = os.getenv("CULTURE_AMP_CLIENT_SECRET")
 
+    if not skip_greenhouse:
+        job_dicts = []
+        if use_applications:
+            applications = get_greenhouse_applications(api_token)
+            print(f"Found {len(applications)} applications")
+            job_dicts = [{"application_id": application["id"], "candidate_id": application["candidate_id"]} for application in applications]
+        else:
+            accepted_offers = get_greenhouse_accepted_offers(api_token)
+            print(f"Found {len(accepted_offers)} accepted offers")
+            job_dicts = [{"application_id": offer["application_id"], "candidate_id": offer["candidate_id"]} for offer in accepted_offers]
+        scorecard_data = {}
+        candidate_data = {}
+        
+        # Function to process a single offer
+        def process_offer(application_id: int):
+            return application_id, get_greenhouse_scorecards(api_token, application_id)
+        
+        # Use ThreadPoolExecutor to parallelize API calls
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            print("Processing offers in parallel...")
+            future_to_offer = {executor.submit(process_offer, job_dict["application_id"]): job_dict for job_dict in job_dicts}
+            
+            completed = 0
+            for future in concurrent.futures.as_completed(future_to_offer):
+                completed += 1
+                if completed % 10 == 0:
+                    print(f"Processed {completed} of {len(job_dicts)} offers")
+                try:
+                    application_id, scorecards = future.result()
+                    scorecard_data[application_id] = scorecards
+                except Exception as exc:
+                    print(f'Offer processing generated an exception: {exc}')
+        
+        candidate_ids = [job_dict["candidate_id"] for job_dict in job_dicts]
+        candidates = get_greenhouse_candidates(api_token, candidate_ids)
+        for candidate in candidates:
+            candidate_data[candidate["id"]] = candidate
+        print(f"Found {len(scorecard_data)} scorecards and {len(candidate_data)} candidates")
+        candidates_to_offers = {}
+        for candidate_id in candidate_ids:
+            if candidate_id not in candidates_to_offers:
+                candidate_name = candidate_data[candidate_id]["first_name"] + " " + candidate_data[candidate_id]["last_name"]
+                candidates_to_offers[candidate_id] = { "name": candidate_name, "offers": 1}
+            else:
+                candidates_to_offers[candidate_id]["offers"] += 1
+        for candidate_id, data in candidates_to_offers.items():
+            if data["offers"] > 1:
+                print(f"{data['name']} has {data['offers']} offers")
+            else:
+                print(f"{data['name']}")
+
+    # fetch data from culture amp
+    culture_amp_employees = fetch_culture_amp_employees(culture_amp_client_id, culture_amp_client_secret)
+    print(f"Found {len(culture_amp_employees)} culture amp employees")
+
+    if not skip_greenhouse:
     # Collate all the data
-    scorecards_data_for_offers = {}
-    for offer in job_dicts:
-        candidate = candidate_data[offer["candidate_id"]]
-        scorecards = scorecard_data[offer["application_id"]]
-        application = next(app for app in candidate["applications"] if app["id"] == offer["application_id"])
-        scorecards_data_for_offers[offer["application_id"]] = {
-            "candidate_name": candidate["first_name"] + " " + candidate["last_name"],
-            "candidate_data": {
-                "company": candidate["company"],
-                "title": candidate["title"],
-                "created_at": candidate["created_at"],
-                "recruiter_name": candidate["recruiter"]["name"] if "recruiter" in candidate and candidate["recruiter"] is not None else None,
-            },
-            "application_id": offer["application_id"],
-            "application_data": {
-                "source": application["source"]["public_name"] if "source" in application and application["source"] is not None else None,
-                "credited_to": application["credited_to"]["name"] if "credited_to" in application and application["credited_to"] is not None else None,
-                "jobs": [job["name"] for job in application["jobs"]] if "jobs" in application and application["jobs"] is not None else [],
-                "prospective_department": application["prospective_department"],
-            },
-            "scorecards_data": [format_scorecard(scorecard) for scorecard in scorecards]
-        }
-    
-    # Write the data to a JSON file
-    with open("scorecards_data.json", "w") as f:
-        json.dump(scorecards_data_for_offers, f)
+        scorecards_data_for_offers = {}
+        for offer in job_dicts:
+            candidate = candidate_data[offer["candidate_id"]]
+            scorecards = scorecard_data[offer["application_id"]]
+            application = next(app for app in candidate["applications"] if app["id"] == offer["application_id"])
+            scorecards_data_for_offers[offer["application_id"]] = {
+                "candidate_name": candidate["first_name"] + " " + candidate["last_name"],
+                "candidate_data": {
+                    "company": candidate["company"],
+                    "title": candidate["title"],
+                    "created_at": candidate["created_at"],
+                    "recruiter_name": candidate["recruiter"]["name"] if "recruiter" in candidate and candidate["recruiter"] is not None else None,
+                },
+                "application_id": offer["application_id"],
+                "application_data": {
+                    "source": application["source"]["public_name"] if "source" in application and application["source"] is not None else None,
+                    "credited_to": application["credited_to"]["name"] if "credited_to" in application and application["credited_to"] is not None else None,
+                    "jobs": [job["name"] for job in application["jobs"]] if "jobs" in application and application["jobs"] is not None else [],
+                    "prospective_department": application["prospective_department"],
+                },
+                "scorecards_data": [format_scorecard(scorecard) for scorecard in scorecards]
+            }
+        
+        # Write the data to a JSON file
+        with open("scorecards_data.json", "w") as f:
+            json.dump(scorecards_data_for_offers, f)
         
